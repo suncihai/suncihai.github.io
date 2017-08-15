0920c2586f57c35804f9bce3346f2697202bf016
cihai sun <cihai sun>
2017-08-14 23:41:43 -0400

    test2

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,3 @@
 (function(){
-   var desaturate = false;
-   desaturator();
 
-   function desaturator(){
-   	  if(!desaturate){
-   	  	 grayscale($(".resume_header"));
-   	  	 desaturate = true;
-   	  }else{
-         grayscale.reset($(".resume_header"));
-         desaturate = false;
-   	  }
-      setTimeout(desaturator,3100);
-   }
 })()
