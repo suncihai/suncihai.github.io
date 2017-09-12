31250f5f38c302b5187a3f87df1f290a73af9d65
cihai sun <cihai sun>
2017-09-11 23:16:45 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -13,4 +13,24 @@
       $(".resume_header-navi li").removeClass("active");
       $(this).addClass("active");
    });
+
+   $(".resume_mobile_header-navi li").click(function(event){
+      $(".resume_mobile_header-navi li").removeClass("active");
+      $(this).addClass("active");
+   });
+
+   var url = window.location.href.split("").reverse();
+   var currenturl="";
+   for(var i=0;i<url.length;i++){
+   	  if(url[i]=="/"){
+      	 break;
+      }
+      currenturl += url[i];
+   }
+   currenturl = currenturl.split("").reverse().join("");
+   if(currenturl==""){
+      $(".homepage").addClass("active");
+   }else{
+      $("."+currenturl+"page").addClass("active");
+   }
 })();
